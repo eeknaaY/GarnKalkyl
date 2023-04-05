45b5302d3e6e9c8cf017915f5daaa38599aa1ad3
Johannes <johanneshageus@hotmail.com>
2023-04-05 16:33:49 +0200

    fixed

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function testing() {
     var width = document.getElementById('Width').value;
     var length = document.getElementById('Length').value;
     var weight = document.getElementById('Weight').value
-    var density = width * length / weight
+    var density = weight / (width * length)
     var desiredwidth = document.getElementById('WantedWidth').value;
     var desiredlength = document.getElementById('WantedLength').value;
 
     document.getElementById('area').innerHTML = 'Area: ' + width * length + ' cm²';
-    document.getElementById('WeightPerArea').innerHTML = 'Gram/cm² : ' + width * length / weight;
+    document.getElementById('WeightPerArea').innerHTML = 'Gram/cm² : ' + weight / (width * length);
     document.getElementById('result').innerHTML = 'Gram som behövs: ' + density * desiredlength * desiredwidth;
 }
